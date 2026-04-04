6ccffbc5f366109c811a2bb509d1cc94633a942d
Jacob Gorton <jgorton200@gmail.com>
2026-04-04 13:11:35 -0700

    Feat: add a health check endpoint to the service

--- app/src/main/java/com/turntable/app/App.java
+++ app/src/main/java/com/turntable/app/App.java
@@ -3,12 +3,14 @@ package com.turntable.app;
 import io.javalin.Javalin;
 
 import static io.javalin.apibuilder.ApiBuilder.crud;
+import static io.javalin.apibuilder.ApiBuilder.get;
 import static io.javalin.apibuilder.ApiBuilder.ws;
 
 public class App {
     public static void main(String[] args) {
         Javalin.create(config -> {
             config.routes.apiBuilder(() -> {
+                get("/health", ctx -> ctx.status(200));
                 crud("/users/{user-id}", new UserController());
                 crud("/friends/{friend-id}", new FriendController());
                 crud("/games/{game-id}", new GameController());
